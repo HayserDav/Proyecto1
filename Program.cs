32cdbe89dfedd49606bf6265bfa16d3333337676
HayserDav <hayser.davila@edu.upolitecnica.cr>
2025-11-19 17:18:51 -0600

    Add files via upload
    Versión con TXT

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using System.IO;
-using System.Windows.Forms;
 using FeriaEnLinea.Application.Controllers;
 using FeriaEnLinea.Infrastructure.Repositories;
 using FeriaEnLinea.Presentation.Views;
 using WinFormsApplication = System.Windows.Forms.Application;
+
 
 namespace FeriaEnLinea.Presentation
 {
@@ -13,18 +13,35 @@ namespace FeriaEnLinea.Presentation
         [STAThread]
         static void Main()
         {
-            ApplicationConfiguration.Initialize();
+            WinFormsApplication.EnableVisualStyles();
+            WinFormsApplication.SetCompatibleTextRenderingDefault(false);
+           
 
             string basePath = AppDomain.CurrentDomain.BaseDirectory;
             string rutaUsuariosTxt = Path.Combine(basePath, "usuarios.txt");
+            string rutaProductosCsv = Path.Combine(basePath, "productos.csv");
 
             var repoUsuarios = new UsuarioFileRepository(rutaUsuariosTxt);
-            var usuarioController = new UsuarioController(repoUsuarios);
+            var repoProductos = new ProductoCsvRepository(rutaProductosCsv);
+            var repoCarritos = new CarritoInMemoryRepository();
 
-            WinFormsApplication.Run(new FrmLogin(usuarioController));
+            var usuarioController = new UsuarioController(repoUsuarios);
+            var carritoController = new CarritoController(repoCarritos, repoProductos);
+
+           
+            using (var frmLogin = new FrmLogin(usuarioController, carritoController))
+            {
+                var resultado = frmLogin.ShowDialog();
+
+                if (resultado == System.Windows.Forms.DialogResult.OK &&
+                    frmLogin.UsuarioAutenticado != null)
+                {
+                    WinFormsApplication.Run(
+                        new FrmPrincipal(frmLogin.UsuarioAutenticado, carritoController));
+                }
+              
+            }
         }
     }
 }
-
-
 
